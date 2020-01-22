582dbaeff620b4ccc69f57acbedd3902cc24a254
Dojun Park <dojun.parkk@gmail.com>
2020-01-22 15:24:15 +0900

    add datatype exercise

---
+++ Tutorial3/src/Main.java
@@ -0,0 +1,32 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//변수 연산
+//		double a = 10.3;
+//		double b = 9.6;
+//		double c = 10.1;
+//		System.out.println((a + b + c) / 3);
+		
+		//for문을 반복하며 문자열 출력
+//		for(char i='a'; i<='z'; i++)
+//		{
+//			System.out.print(i + " ");
+//		}
+
+		//10진수 -> 8진수, 16진수
+//		int a = 200;
+//		System.out.println("10진수: " + a);
+//		System.out.format("8진수: %o\n", a);
+//		System.out.format("16진수: %x\n", a);
+
+		String name = "Dojun";
+		System.out.println(name);
+		System.out.println(name.substring(0, 1));
+		System.out.println(name.substring(2, 5));
+		System.out.println(name.substring(2));
+		
+	}
+
+}
