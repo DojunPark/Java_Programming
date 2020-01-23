24f0b6d415ee041fe54632affea8afec48ba2e7f
Dojun Park <dojun.parkk@gmail.com>
2020-01-23 17:06:40 +0900

    add summary exercise

---
+++ Tutorial8/src/Main.java
@@ -0,0 +1,26 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//출력하기
+		System.out.println("박도준");
+		
+		//코드 실행
+		System.out.println(10 + 10);
+		System.out.println(30 * 30);
+		System.out.println(20 - 5);
+		System.out.println(40 / 3);
+		System.out.println(395 / 18);
+		
+		//사각형모양의 별 출력
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++ ) {
+				System.out.print("*");	
+			}
+			System.out.println("");
+		}
+		
+	}
+
+}
