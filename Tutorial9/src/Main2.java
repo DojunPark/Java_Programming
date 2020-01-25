7dcb76be732c291ebbd57b3339e08f15bf4b1e06
Dojun Park <dojun.parkk@gmail.com>
2020-01-25 16:12:43 +0900

    add function exercise

---
+++ Tutorial9/src/Main2.java
@@ -0,0 +1,15 @@
+
+public class Main2 {
+	
+	//function returning last char of input String 
+	public static char function(String input) {
+		return input.charAt(input.length()-1);
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World의 마지막 단어는 " + function("Hello World"));
+
+	}
+
+}
